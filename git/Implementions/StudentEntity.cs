0b37d7c1c9b2b49ffcc472bb27b9e20ce1c03219
AkulasIKEA <131446329+AkulasIKEA@users.noreply.github.com>
2024-03-23 18:32:02 +0200

    add code to StudentsEntity and create test class
    в тестовому класі перевіряємо, що об'єкт не є пустим та перевіряємо наслідування

--- git/Implementions/StudentEntity.cs
+++ git/Implementions/StudentEntity.cs
@@ -2,5 +2,6 @@ namespace Groups.Implementions;
 
 public class StudentEntity : PersonEntity
 {
-
+    public int StudentID { get; set; }
+    public string Major { get; set; }
 }
---
+++ git/Tests/StudentEntityTests.cs
@@ -0,0 +1,31 @@
+﻿namespace Students.Tests
+{
+    [TestClass]
+    public class StudentEntityTests
+    {
+        [TestMethod]
+        public void StudentEntity_Initialization_ShouldSucceed()
+        {
+            // Arrange
+            var student = new Students.Entities.StudentEntity
+            {
+                Name = "John Doe",
+                Age = 20,
+                StudentID = 123456,
+                Major = "Computer Science"
+            };
+
+            // Act
+            string expectedName = "John Doe";
+            int expectedAge = 20;
+            int expectedStudentID = 123456;
+            string expectedMajor = "Computer Science";
+
+            // Assert
+            Assert.AreEqual(expectedName, student.Name);
+            Assert.AreEqual(expectedAge, student.Age);
+            Assert.AreEqual(expectedStudentID, student.StudentID);
+            Assert.AreEqual(expectedMajor, student.Major);
+        }
+    }
+}
